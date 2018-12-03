9b49f946227010ef2e3490c11bfd46b7e03e4809
Hampus Widén <hampus.widen@hotmail.com>
2018-12-03 11:53:04 +0100

    Makefile + creatures

---
+++ pollax/creatures/Creature.java
@@ -0,0 +1,18 @@
+package pollax.creatures;
+
+/**
+ * @author      Jonathan Franzén, Hampus Widén
+ * @version     1.0
+ * @since       1.0
+ */
+public class Creature {
+	private String name = "creature";
+
+	public static void main(String[] args) {
+		System.out.println("hey");
+	}
+
+	public boolean isAvatar() {
+		return false;
+	}
+}
---
+++ pollax/creatures/Teacher.java
@@ -0,0 +1,14 @@
+package pollax.creatures;
+
+/**
+ * @author      Jonathan Franzén, Hampus Widén
+ * @version     1.0
+ * @since       1.0
+ */
+public class Teacher extends Creature {
+	private String name;
+
+	public Teacher(String name) {
+		this.name = name;
+	}
+}
