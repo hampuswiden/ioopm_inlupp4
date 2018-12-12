a6a6f3b5ed06164ab975e4f1bf65a88d9ce1cc0f
Jonathan Franzen <jonathan.franzeen@gmail.com>
2018-12-12 21:38:16 +0100

    finished docs

--- pollax/world/WorldUtils.java
+++ pollax/world/WorldUtils.java
@@ -20,19 +20,37 @@ import pollax.world.Room;
 public class WorldUtils {
 	private Random rand = new Random();
 
+	/**
+	* empty constructor for WorldUtils.
+	*/
 	public WorldUtils() {
 
 	}
 
+	/**
+	* checks the size of any HashMap.
+	* @param db to check size of.
+	* @return size of input HashMap/db.
+	*/
 	public <A,B> int size(HashMap<A,B> db) {
 		return db.size();
 	}
 
+	/**
+	* Generates a random number 0-100 and compares it to input int.
+	* @param chance int to compare with random generated int.
+	* @return true if input chance is higher than random one.
+	*/
 	public boolean random(int chance) {
 		int n = this.rand.nextInt(100);
 		return n < chance;
 	}
 
+	/**
+	* gets a random room from the room database.
+	* @param db the HashMap (database) to get room from.
+	* @return the randomly picked room.
+	*/
 	public Room randomRoom(HashMap<String, Room> db) {
 		int n = this.rand.nextInt(db.size()); // this.size() is the maximum and 0 is our minimum.
 
@@ -46,6 +64,12 @@ public class WorldUtils {
 		return new Room();
 	}
 
+	/**
+	* checks for the number of locked doors in Room database.
+	* Note: a "locked door" is only locked one-way.
+	* @param db the HashMap of rooms the check doors in.
+	* @return number of locked doors.
+	*/
 	public int noLockedDoors(HashMap<String, Room> db) {
 		int noLockedDoors = 0;
 		String[] directions = {"north", "east", "south", "west"};
